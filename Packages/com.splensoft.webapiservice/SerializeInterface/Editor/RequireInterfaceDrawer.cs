07c2ae617812b02c9492ad602f3ad67bd4bdfb4e
SplenectomY <skaterjohn16@hotmail.com>
2026-03-14 00:18:30 -0400

    Wrap editor scripts in UNITY_EDITOR, bump version to 2.0.7
    Wrapped InterfaceReference* scripts in UNITY_EDITOR guards to prevent inclusion in builds. Moved editor-only using directives inside guards. Updated package.json version to 2.0.7.

--- Packages/com.splensoft.webapiservice/SerializeInterface/Editor/RequireInterfaceDrawer.cs
+++ Packages/com.splensoft.webapiservice/SerializeInterface/Editor/RequireInterfaceDrawer.cs
@@ -1,3 +1,4 @@
+#if UNITY_EDITOR
 using System;
 using System.Collections.Generic;
 using UnityEditor;
@@ -84,3 +85,4 @@ internal class RequireInterfaceDrawer : PropertyDrawer {
         return type;
     }
 }
+#endif
